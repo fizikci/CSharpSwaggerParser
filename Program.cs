b59f2fe6c596707248c98af284c6a5cb629244f4
Bulent Keskin <bulentkeskin@gmail.com>
2022-03-15 02:03:50 -0700

    test code

--- Program.cs
+++ Program.cs
@@ -1,59 +1,16 @@
-﻿using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
+﻿using System.Net;
 
-var path = @"C:\Users\buk\Downloads\rapid-v24.yaml";
+var swaggerUrl = @"http://rackerlabs.github.io/wadl2swagger/openstack/swagger/os-hosts.yaml";
+var content = new WebClient().DownloadString(swaggerUrl);
 
-var deserializer = new DeserializerBuilder()
-    .WithNamingConvention(new UnderscoredNamingConvention())
-    .Build();
-
-var x = deserializer.Deserialize<dynamic>(File.ReadAllText(path));
-
-var res = setProps(typeof(Swagger), x) as Swagger;
-
-Console.WriteLine("end");
+var res = Swagger.Parse(content);
 
 
-dynamic setProps(Type type, dynamic props)
+foreach (var path in res.paths)
 {
-    if (type == typeof(string))
-        return props;
-
-    if (type == typeof(bool))
-        return props;
-
-    if (type == typeof(int))
-        return props;
-
-    if (type.FullName == "System.Object")
-        return props;
-
-    dynamic obj = Activator.CreateInstance(type);
-
-    if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
-        foreach (var v in props)
-            obj.Add(setProps(type.GetGenericArguments()[0], v));
-    else if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Dictionary<,>))
-        foreach (var kv in props)
-            obj.Add(kv.Key, setProps(type.GetGenericArguments()[1], kv.Value));
-    else if (type.Name.Contains("Dictionary"))
-        foreach (var kv in props)
-            obj.Add(kv.Key, setProps(kv.Value.GetType(), kv.Value));
-    else if (type.Name.Contains("List"))
-        foreach (var v in props)
-            obj.Add(setProps(v.GetType(), v));
-    else
-        foreach (var kv in props)
-        {
-            var propName = kv.Key == "$ref" ? "refers" : kv.Key;
-            var propVal = kv.Value;
-
-            var prop = obj.GetType().GetProperty(propName);
-            if (prop == null) prop = obj.GetType().GetProperty("_" + propName);
-            if (prop == null) continue;
-
-            prop.SetValue(obj, setProps(prop.PropertyType, propVal));
-        }
-
-    return obj;
+    if (path.Value.get != null) Console.WriteLine("GET " + path.Key);
+    if (path.Value.post != null) Console.WriteLine("POST " + path.Key);
+    if (path.Value.put != null) Console.WriteLine("PUT " + path.Key);
 }
+
+
